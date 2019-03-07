04cbb737a3f7ed4a5cfe744afd230eb99a69c098
Sebastian L Vaughan P776491 <Sebastian.Vaughan@nab.com.au>
2019-03-07 23:09:55 +1100

    fix: removed stuff, now using comma instead of str

--- variables.py
+++ variables.py
@@ -1,38 +1,27 @@
-# Variables, Strings, Basic Addition and Concatenation
+# Variables, Strings, Operators and Concatenation
 
 name = 'Harry Potter'
 age = 21
 isStudent = True # Notice how 'Student' is capitalized whilst 'is' isn't? (This is called camelCasing!)
-print('My name is ' + name)
-print('I am ' + str(age) + ' years old')
-print('Am i a student? ' + str(isStudent))
-# works in python 2 and 3
-print('Am i a student?', str(isStudent))
+print('My name is', name)
+print('I am', age, 'years old')
+print('Am I a student?', isStudent)
 
 
 # We can add the values of variables together
 num1 = 10
 num2 = 20
-print(num1 + num2)
+print("10 + 20 =", num1 + num2)
 # Subtract the values
-print(num1 - num2)
+print("10 - 20 =", num1 - num2)
 # Multiply the values
-print(num1 * num2)
-# Divide the values (and much more!)
-print(num1 / num2)
-
-# We can also add strings together
-firstName = "Harry"
-lastName = "Potter"
-fullName = firstName + lastName
-# works in python 2 and 3
-print(fullName)
-
-# We can re-assign values to our variables
-myFavouriteNumber = 9
-myFavouriteNumber = 5
-print(myFavouriteNumber)
+print("10 * 20 =", num1 * num2)
+# Divide the values - left divided by right (and much more!)
+print("10 / 20 =", num1 / num2)
+# Modulus, gives remainder of the divided values
+print("10 % 20 =", num1 % num2)
+# Returns True if the two values are equal (otherwise returns False)
+print("10 - 20 =", num1 == num2)
 
 # TypeError: cannot concatenate 'str' and 'int' objects
 # print('I am ' + age + ' years old')
-

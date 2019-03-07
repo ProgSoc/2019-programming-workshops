a217235de58d0645f331d991b9d41545da37ae28
Sebastian L Vaughan P776491 <Sebastian.Vaughan@nab.com.au>
2019-03-07 23:11:57 +1100

    feat: concatenate with plus example

--- variables.py
+++ variables.py
@@ -4,6 +4,8 @@ name = 'Harry Potter'
 age = 21
 isStudent = True # Notice how 'Student' is capitalized whilst 'is' isn't? (This is called camelCasing!)
 print('My name is', name)
+# You can also concatenate strings with '+', but python only concatenates strings when you use the '+' symbol (and it doesn't add a space)
+print('My name is ' + name)
 print('I am', age, 'years old')
 print('Am I a student?', isStudent)
 

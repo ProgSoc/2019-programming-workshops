62682a098b35214e365f472e4b68952f918bf3fd
Sebastian L Vaughan P776491 <Sebastian.Vaughan@nab.com.au>
2019-03-07 22:51:11 +1100

    fix: added operators and fixed syntax

--- variables.py
+++ variables.py
@@ -6,24 +6,32 @@ isStudent = True # Notice how 'Student' is capitalized whilst 'is' isn't? (This 
 print('My name is ' + name)
 print('I am ' + str(age) + ' years old')
 print('Am i a student? ' + str(isStudent))
-print 'Am i a student?', str(isStudent)
+# works in python 2 and 3
+print('Am i a student?', str(isStudent))
 
 
 # We can add the values of variables together
 num1 = 10
 num2 = 20
 print(num1 + num2)
+# Subtract the values
+print(num1 - num2)
+# Multiply the values
+print(num1 * num2)
+# Divide the values (and much more!)
+print(num1 / num2)
 
 # We can also add strings together
 firstName = "Harry"
 lastName = "Potter"
 fullName = firstName + lastName
-print fullName
+# works in python 2 and 3
+print(fullName)
 
 # We can re-assign values to our variables
 myFavouriteNumber = 9
 myFavouriteNumber = 5
-print myFavouriteNumber
+print(myFavouriteNumber)
 
 # TypeError: cannot concatenate 'str' and 'int' objects
 # print('I am ' + age + ' years old')

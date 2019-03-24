2a9a831900dcd177cf36377b40db5aae186a590d
bazerama <sebvaughan97@gmail.com>
2019-03-24 23:08:36 +1100

    moved operators content to operators.py

---
+++ workshop-1/operators.py
@@ -0,0 +1,23 @@
+# basic operators
+
+# Subtract the values
+print("10 - 20 =", num1 - num2)
+# Multiply the values
+print("10 * 20 =", num1 * num2)
+# Divide the values - left divided by right (and much more!)
+print("10 / 20 =", num1 / num2)
+# Modulus, gives remainder of the divided values
+print("10 % 20 =", num1 % num2)
+
+# comparison operators
+
+# Returns True if the two values are equal (otherwise returns False)
+print("Is 10 == 20 ?", num1 == num2)
+# Returns True if left is greater than right (otherwise returns False)
+print("Is 10 > 20 ?", num1 > num2)
+# Returns True if left is less than right (otherwise returns False)
+print("Is 10 < 20 ?", num1 < num2)
+# Returns True if left is greater than or equal to right (otherwise returns False)
+print("Is 10 >= 20 ?", num1 >= num2)
+# Returns True if left is less than or equal to right (otherwise returns False)
+print("Is 10 <= 20 ?", num1 <= num2)

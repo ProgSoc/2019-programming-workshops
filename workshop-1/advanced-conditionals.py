3656704b7fcfffbd172b4294d794fd6a69b2426a
bazerama <sebvaughan97@gmail.com>
2019-03-24 23:29:44 +1100

    added advanced-conditionals activity

--- workshop-1/advanced-conditionals.py
+++ workshop-1/advanced-conditionals.py
@@ -15,3 +15,18 @@ if vehicle != "motorbike" and brand == "Mercedes":
         print("You're driving a German truck")
     elif vehicle == "car":
         print("You're driving a German car")
+
+# ACTIVITY TIME!! :) (5-15 min depending on skill level)
+
+# try to finish off my function...
+print("I can't figure out how to check where the groceries should go!")
+# given this list of groceries:
+groceries = ["cereal", "milk", "beef steak", "ice cream", "detergent"]
+
+# for reference, cereal = room temperature, milk = cold, beef steak = cold, ice cream = freezing, detergent = other
+# remember that milk and eggs can't go with the beef steaks, and detergent should have it's own storage
+# bonus points for printing this out too!!!
+
+# create a set of if/elif/else statements that print() the type of grocery and storage location of each item!
+# you can print the storage location based on their storageTemp (room temperature, cold, freezing, other)
+# the most efficient statement will have less lines (logic) and be easily readable, GOOD LUCK!

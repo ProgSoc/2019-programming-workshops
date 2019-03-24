cbdaf533ff15ddc319bf0049fde1f75a7c287e84
bazerama <sebvaughan97@gmail.com>
2019-03-24 23:16:37 +1100

    moved functions content to functions.py
    moved functions content to functions.py

---
+++ workshop-1/functions.py
@@ -0,0 +1,9 @@
+# functions - perform basic operations using a function
+
+# returns the power of two numbers using the in-built pow() function
+def powerOf(num1, num2):
+    return pow(num1,num2)
+
+# returns a full name, given the parameters first name and last name
+def getFullName(firstName, lastName):
+    return firstName + " " + lastName
